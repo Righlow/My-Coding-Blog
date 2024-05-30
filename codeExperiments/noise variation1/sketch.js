6a9e34b306be25231caab18ea1c43ae3bbe1c4e8
Righlow <146979400+Righlow@users.noreply.github.com>
2024-05-30 12:47:00 +0100

    Add files via upload

---
+++ codeExperiments/noise variation1/sketch.js
@@ -0,0 +1,29 @@
+let step
+let numAcross=150
+
+function setup() {
+  createCanvas(500,500);
+  step=width/numAcross
+}
+
+
+function draw() {
+  background(220);
+  
+  for(let j=0; j<numAcross; j++){
+    for(let i=0; i<numAcross; i++){
+      drawTile(i,j,step)
+      }
+  } 
+}
+
+function drawTile(across, down, step){
+  push()
+  translate((across +0.5)*step, (down+0.5)*step)
+  rectMode(CENTER)
+ // angleMode(DEGREES)
+  rotate(frameCount+across*down)
+  rect(across*step, down*step, across*step )
+  pop()
+  
+}
